30b0cc16b4680ec87b07572628daa1a3a08716cb
Rajuuu001 <arputha@neophrontech.com>
2020-10-29 15:39:10 +0530

    Initial commit

---
+++ app/src/main/assets/scripts/firebase.js
@@ -0,0 +1,14 @@
+   // Initialize Firebase
+ var config = {
+      apiKey: "AIzaSyCus5Oip4KMYGZBX1olo8pRDLoxzI2Tbt4",
+    authDomain: "erodecityapp-41f0a.firebaseapp.com",
+    databaseURL: "https://erodecityapp-41f0a.firebaseio.com",
+    projectId: "erodecityapp-41f0a",
+    storageBucket: "erodecityapp-41f0a.appspot.com",
+    messagingSenderId: "328460040116"
+  };
+  firebase.initializeApp(config);
+
+var BASE_URL = 'http://localhost/erodecityapp/';
+var STORAGE_URL = 'https://firebasestorage.googleapis.com/v0/b/erodecityapp-41f0a.appspot.com/o/';
+var WEB_TITLE = 'City App';
